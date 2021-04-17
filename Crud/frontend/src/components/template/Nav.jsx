b78970ab4ba113ce0d4e811ab59c1244ff434e31
charles oliveira <charlesww1@hotmail.com>
2021-04-17 20:57:33 -0300

    Nav-menu concluido

--- Crud/frontend/src/components/template/Nav.jsx
+++ Crud/frontend/src/components/template/Nav.jsx
@@ -3,5 +3,17 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegaçâo
+        <nav className="menu">
+            {/*Refatorar depois! Nav-item*/}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuário
+            </a>
+            <a href='#/contato'>
+                <i className="fa fa-id-card"></i> Contato
+            </a>
+
+        </nav>
     </aside>
